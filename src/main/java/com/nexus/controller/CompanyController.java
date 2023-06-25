629b7ac3e953b40f7295d810a6af9a19b16c767c
Karim Ahmed <karim.ahmed@dxc.com>
2023-06-25 12:20:53 +0300

    @bean

--- src/main/java/com/nexus/controller/CompanyController.java
+++ src/main/java/com/nexus/controller/CompanyController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("api")
 @Validated
-@CrossOrigin(origins = "https://nexusgrows-97ac5.web.app")
+@CrossOrigin(origins = "*")
 
 @SuppressWarnings("all")
 public class CompanyController {
